419aa15320d65e57153418a44cd62a182fe44fe1
Amaal Ali <amq.ali@gmail.com>
2016-09-23 22:22:44 +0100

    Added ability to load students details from students.csv file

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 #students = [{name: "Dr. Hannibal Lecter", cohort: :november}, {name: "Darth Vader", cohort: :november}, {name: "Nurse Ratched", cohort: :november}, {name: "Michael Corloene", cohort: :november}, {name: "Alex DeLarge", cohort: :november}, {name: "The Wicked Witch of the West", cohort: :november}, {name: "Terminator", cohort: :november}, {name: "Freddy Krueger", cohort: :november}, {name: "The Joker",  cohort: :november}, {name: "Joffrey Baratheon", cohort: :november}, {name: "Norman Bates", cohort: :november}]
+@students = []
 
 def print_header
   heading = "The students of Villian Academy"
@@ -60,6 +61,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -80,6 +82,16 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students <<  {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts @students
+end
+
 def interactive_menu
   @students = Array.new
   loop do
@@ -95,6 +107,8 @@ def interactive_menu
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
